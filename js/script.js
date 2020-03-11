2922e74ef413396f495c984a88f3eeacc9689e16
Jaskiran <jaskiranchhokar@gmail.com>
2020-03-11 00:13:50 -0400

    Clean up comments

--- js/script.js
+++ js/script.js
@@ -1,40 +1,8 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-
+//Declare Global Variables
 const studentList = document.querySelectorAll('.student-item');
 const numberOfItems = 10; 
 
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
+//Create Show Page Function
 
 function showPage(list, page) {
    const startIndex = (page * numberOfItems) - numberOfItems; 
@@ -49,11 +17,7 @@ function showPage(list, page) {
    }
 }
 
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
+// Create Append Page Links Function
 
 function appendPageLinks(list) {
    const page = document.querySelector('.page');
@@ -85,7 +49,3 @@ function appendPageLinks(list) {
 
 showPage(studentList, 1);
 appendPageLinks(studentList);
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
