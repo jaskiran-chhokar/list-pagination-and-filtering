76405f332b9dab06fa69df770e15f397f3771fc8
Jaskiran <jaskiranchhokar@gmail.com>
2020-03-11 00:06:01 -0400

    add appendPageLinks function

--- js/script.js
+++ js/script.js
@@ -39,14 +39,15 @@ const numberOfItems = 10;
 function showPage(list, page) {
    const startIndex = (page * numberOfItems) - numberOfItems; 
    const endIndex = page * numberOfItems; 
+
    for(let i = 0; i < list.length; i++) {
       if(i >= startIndex && i < endIndex) {
-         console.log(list[i]);
+         list[i].style.display = 'block';
+      } else {
+         list[i].style.display = 'none';
       }
    }
 }
-
-showPage(studentList, 1);
 
 
 /*** 
@@ -54,7 +55,36 @@ showPage(studentList, 1);
    functionality to the pagination buttons.
 ***/
 
+function appendPageLinks(list) {
+   const page = document.querySelector('.page');
+   const pagination = document.createElement('div'); 
+   const ul = document.createElement('ul');
+   pagination.classList.add('pagination');
+   page.appendChild(pagination);
+   pagination.appendChild(ul);
 
+   for(let i = 0; i <= (list.length / numberOfItems); i++) {
+      ul.innerHTML += `<li><a href="#">${i}</a></li>`; 
+   }
+
+   ul.firstElementChild.firstElementChild.className = 'active';
+   
+   const anchor = document.querySelectorAll('.pagination a'); 
+   pagination.addEventListener('click', (e) => {
+      for(let i = 0; i < anchor.length; i++) {
+         if(e.target === anchor[i]) {
+            anchor[i].classList.add('active');
+            showPage(studentList, i);
+         } else {
+            anchor[i].classList.remove('active');
+         }
+      }
+      e.preventDefault();
+   });
+}
+
+showPage(studentList, 1);
+appendPageLinks(studentList);
 
 
 
